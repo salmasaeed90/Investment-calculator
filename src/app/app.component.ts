4866b125bb98e6439e1fca4ec4894e1b305ca695
salmasaeed90 <salmaasaeed90@gmail.com>
2024-07-13 00:02:40 +0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,22 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { HeaderComponent } from './header/header.component';
-import { InvestmentComponent } from './investment/investment.component';
 import { InputData } from './investment.modal';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormComponent, HeaderComponent, InvestmentComponent],
+  imports: [RouterOutlet, FormComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  calculator!:InputData;
  
-
-  investmentData(formData:InputData){
-    this.calculator = formData;
-  console.log(this.calculator);
-  }
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,31 +1,23 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CurrencyPipe],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
-  initialInvestment = signal('0')
-  annualInvestment= signal('0');
-  expectedReturn= signal('0');
-  duration= signal('0');
-
-  formData = output<{
-    initialInvestment:string,
-      annualInvestment:string,
-      expectedReturn:string,
-      duration:string,
-  }>();
-
-
-
+  initialInvestment = signal('')
+  annualInvestment= signal('');
+  expectedReturn= signal('');
+  duration= signal('');
+  calculator!:any;
+  calculatResult!:any[];
 
   onSubmit(){
-    console.log('checked');
     
     const formData = {
       initialInvestment:this.initialInvestment(),
@@ -36,6 +28,45 @@ export class FormComponent {
     }
     console.log(formData);
     
-    this.formData.emit(formData)
+    this.calculator = formData;
+    this.createCalculatResult();
+    this.initialInvestment.set('')
+    this.annualInvestment.set('');
+    this.expectedReturn.set('');
+    this.duration.set('');
+  }
+  
+ 
+  ngOnInit(){
+    
+    this.createCalculatResult();
+  }
+  
+
+  createCalculatResult(){
+    const annualInvestmentData = this.calculator?.annualInvestment;
+    const durationData = this.calculator?.duration;
+    const expectedReturnData = this.calculator?.expectedReturn;
+    let initialInvestmentData = this.calculator?.initialInvestment;
+    const resultData = [];
+
+    for(let i = 0; i < Number(durationData); i++){
+      const durationNum = i + 1;
+      const interestEarnedInYear= Number(initialInvestmentData) *(Number(expectedReturnData)/100);
+      initialInvestmentData +=  interestEarnedInYear+ annualInvestmentData!;
+      const interest = 
+      Number(initialInvestmentData)+ Number(annualInvestmentData) * Number(durationData) - Number(initialInvestmentData);
+
+      resultData.push({
+        year: durationNum,
+        interest: interestEarnedInYear,
+        valueEndOfYear: initialInvestmentData,
+        annualInvestment: annualInvestmentData,
+        totalInterest: interest,
+        totalAmountInvested: Number(initialInvestmentData) + Number(annualInvestmentData) * Number(durationNum),
+      })
+      this.calculatResult= resultData;
+      console.log(this.calculatResult);
+    }
   }
 }
